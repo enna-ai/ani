dcbf31d2100ebb04e5029c814a0783d8dd8c5012
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-03 15:54:39 -0700

    initial commit

---
+++ server/src/main/java/vian_ai/aniview/AniviewApplication.java
@@ -0,0 +1,13 @@
+package vian_ai.aniview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniviewApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/vian_ai/aniview/AniviewApplicationTests.java
@@ -0,0 +1,13 @@
+package vian_ai.aniview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
