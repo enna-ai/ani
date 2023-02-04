bab082cee5c600be0c7a2fc759ef0da130645915
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-04 13:44:12 -0700

    add fetch request with axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useState, useEffect} from 'react';
+import api from './Api/axiosConfig';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+      console.log(response.data);
+
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    };
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div className="App">
-      
+      <h1>Hello World</h1>
     </div>
   );
 }
