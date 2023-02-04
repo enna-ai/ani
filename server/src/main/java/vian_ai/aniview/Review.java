443ef033035661b01c58c8c20809a586d4e1a379
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-03 18:45:24 -0700

    add review controller, repository, and service

--- server/src/main/java/vian_ai/aniview/Review.java
+++ server/src/main/java/vian_ai/aniview/Review.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
