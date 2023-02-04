d5d9a8c549f9595ab4d5625e7a665721c68ae610
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-03 17:40:38 -0700

    Add anime class for storing movie data in mongodb using spring data

---
+++ server/src/main/java/vian_ai/aniview/Anime.java
@@ -0,0 +1,26 @@
+package vian_ai.aniview;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Anime {
+    @Id
+    private Object movId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
---
+++ server/src/main/java/vian_ai/aniview/AnimeApplication.java
@@ -0,0 +1,21 @@
+package vian_ai.aniview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class AnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World!";
+	}
+}
