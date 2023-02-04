7aa619e5522d926aab0e7717e98c4e633e3e6e3a
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-03 17:46:41 -0700

    Add review class for storing review data

--- server/src/main/java/vian_ai/aniview/Anime.java
+++ server/src/main/java/vian_ai/aniview/Anime.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,4 +24,6 @@ public class Anime {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<String> reviewIds;
 }
